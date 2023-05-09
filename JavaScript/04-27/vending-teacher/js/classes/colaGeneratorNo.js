4fbf58a0c88f3cd13b15e3a95580d57bf54ff382
DayTeaJun <gkfnck@gmail.com>
2023-05-09 20:29:36 +0900

    230509 React Event

---
+++ JavaScript/04-27/vending-teacher/js/classes/colaGeneratorNo.js
@@ -0,0 +1,45 @@
+// 콜라만 생성하는 JS
+class ColaGenerator {
+  constructor() {
+    this.itemList = document.querySelector(".section1 .cola-list");
+  }
+
+  async setup() {
+    const response = await this.loadData();
+    this.colaFactory(response);
+  }
+
+  async loadData() {
+    try {
+      const response = await fetch("./items.json");
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error(response.status);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  colaFactory(data) {
+    const docFrag = document.createDocumentFragment();
+
+    data.forEach((el) => {
+      const item = document.createElement("li");
+      const itemTemplate = `
+            <button class="btn-cola" type="button" data-item="${el.name}" data-count="${el.count}" data-price="${el.cost}" data-img="${el.img}">
+                <img class="cola-img" src="./img/${el.img}" alt="">
+                <span class="cola-name">${el.name}</span>
+                <strong class="cola-price">${el.cost}원</strong>
+            </button>
+            `;
+
+      item.innerHTML = itemTemplate;
+
+      docFrag.append(item);
+    });
+    this.itemList.append(docFrag);
+  }
+}
+
+export default ColaGenerator;
--- React/05-02/007.js
+++ React/05-02/007.js
@@ -2,41 +2,39 @@ const docmContainer = document.querySelector("#root");
 const root = ReactDOM.createRoot(docmContainer);
 
 async function setUp() {
-    const response = await loadData();
-    wenivPost(response);
-    // console.log(response);
+  const response = await loadData();
+  wenivPost(response);
+  // console.log(response);
 }
 
 async function loadData() {
-    try {
-        const response = await fetch('http://test.api.weniv.co.kr/mall');
-        if (response.ok) {
-            return response.json();
-        } else {
-            throw new Error(response.status)
-        }
-    } catch (error) {
-        console.log(error)
+  try {
+    const response = await fetch("http://test.api.weniv.co.kr/mall");
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error(response.status);
     }
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-
-
 function wenivPost(data) {
-    const dataEnd = [];
-    data.map((el) => {
-        dataEnd.push(body(el));
-    })
-    root.render(dataEnd);
+  const dataEnd = [];
+  data.map((el) => {
+    dataEnd.push(body(el));
+  });
+  root.render(dataEnd);
 }
 
 function body(data) {
-    return (
-        <div>
-            <h1>{data.productName}</h1>
-            <p>{data.price}</p>
-        </div>
-    )
+  return (
+    <div>
+      <h1>{data.productName}</h1>
+      <p>{data.price}</p>
+    </div>
+  );
 }
 
 setUp();
