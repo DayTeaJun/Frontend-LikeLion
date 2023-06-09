d3e54337bc54b16116fcc862a48d90912ea57c79
DayTeaJun <gkfnck@gmail.com>
2023-06-09 09:15:04 +0900

    Comment: 주석제거 주석제거

--- Firebase/mydiary/src/hooks/useCollection.js
+++ Firebase/mydiary/src/hooks/useCollection.js
@@ -9,7 +9,6 @@ export const useCollection = (transaction) => {
   useEffect(() => {
     // 현재 최신 데이터베이스를 가져오고,
     // 데이터베이스 업데이트가 될때마다 콜백이 호출되어 스냅샷을 업데이트함.
-    //
     const unsubscribe = onSnapshot(
       collection(appFireStore, transaction),
       (snapshot) => {
